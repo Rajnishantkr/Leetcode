6244b8f1a1d9823d6871e07b0090ec4650fa1b6f
Rajnishant kumar <rajnishant87827049@gmail.com>
2026-09-20 14:52:58 +0530

    [46] [Time Beats: 100.00%] [Memory Beats: 70.46%] - LeetPush

--- 46-Permutations.cpp
+++ 46-Permutations.cpp
@@ -1,36 +1,26 @@
 class Solution {
 public:
+     void checkAns(int idx,vector<int>& nums,vector<vector<int>>& ans )
+     {
+        if(idx == nums.size())
+          {
+            ans.push_back(nums);
+            return;
+          }
 
- void CheckAns(vector<int>& nums, vector<vector<int>>& ans,vector<int>& tempDS,int freq[])
-  {
-    if(tempDS.size() == nums.size())
-    {
-       ans.push_back(tempDS);
-       return ;
-       }
-
-       for(int i=0; i< nums.size(); i++)
-       {
-        if(freq[i] != 1)
-        {
-            freq[i]=1;
-            tempDS.push_back(nums[i]);
-
-            CheckAns(nums,ans,tempDS,freq);
-            freq[i] = 0;
-            tempDS.pop_back();
-        }
-       }
-   }
+          for (int i=idx;i<nums.size(); i++)
+          {
+            swap(nums[idx],nums[i]);
+            checkAns(idx+1,nums, ans );
+            swap(nums[idx],nums[i]);
+            }
+      }
 
     vector<vector<int>> permute(vector<int>& nums) {
         vector<vector<int>> ans;
-        vector<int> tempDS;
-        int freq[nums.size()];
-         for(int i=0; i< nums.size(); i++)
-           freq[i]=0;
-        CheckAns(nums,ans,tempDS,freq);
 
-        return ans;
+        checkAns(0,nums, ans );
+
+        return  ans;
     }
 };
